a6aeddf8d7eed5f3fe9ba0e0d0bff867dfd9e4dc
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-08-11 00:17:24 +0200

    Use BufWriter in line examples

--- src/bin/bstr_for_byte_line_utf8.rs
+++ src/bin/bstr_for_byte_line_utf8.rs
@@ -7,13 +7,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let test_file = std::fs::File::open(test_file_path)?;
     let stdout = std::io::stdout();
     // Lock the stdout once so we don't have to acquire the lock for every line written, which is slow
-    let mut stdout = stdout.lock();
+    let stdout = stdout.lock();
+    // And wrap it in a BufWriter, because we're going to make lots of small writes
+    // and without this it would be very slow
+    let mut writer = std::io::BufWriter::new(stdout);
 
     let mut reader = std::io::BufReader::new(test_file);
     reader.for_byte_line(|line| {
         let _validated_utf8: Result<&str, std::str::Utf8Error> = std::str::from_utf8(line);
         let sum: u64 = line.iter().map(|i| *i as u64).sum();
-        write!(stdout, "{sum}")?;
+        write!(writer, "{sum}")?;
         Ok(true)
     })?;
 
--- src/bin/lines.rs
+++ src/bin/lines.rs
@@ -6,12 +6,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let test_file = std::fs::File::open(test_file_path)?;
     let stdout = std::io::stdout();
     // Lock the stdout once so we don't have to acquire the lock for every line written, which is slow
-    let mut stdout = stdout.lock();
+    let stdout = stdout.lock();
+    // And wrap it in a BufWriter, because we're going to make lots of small writes
+    // and without this it would be very slow
+    let mut writer = std::io::BufWriter::new(stdout);
 
     let reader = std::io::BufReader::new(test_file);
     for line in reader.lines() {
         let sum: u64 = line?.as_bytes().iter().map(|i| *i as u64).sum();
-        writeln!(stdout, "{sum}")?;
+        writeln!(writer, "{sum}")?;
     }
 
     Ok(())
