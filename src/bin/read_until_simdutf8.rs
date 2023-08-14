9defd3a7ef99c1dd379d13e7d1f433266e666323
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-08-14 02:22:13 +0200

    Add a read_until simdutf8 example

---
+++ src/bin/read_until_simdutf8.rs
@@ -0,0 +1,30 @@
+use std::io::{Write, BufRead};
+use simdutf8; // external crate for faster UTF-8 validation
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let test_file_path = std::env::args_os().nth(1).expect("No input file specified!");
+
+    let test_file = std::fs::File::open(test_file_path)?;
+    let stdout = std::io::stdout();
+    // Lock the stdout once so we don't have to acquire the lock for every line written, which is slow
+    let stdout = stdout.lock();
+    // And wrap it in a BufWriter, because we're going to make lots of small writes
+    // and without this it would be very slow
+    let mut writer = std::io::BufWriter::new(stdout);
+
+    let mut reader = std::io::BufReader::with_capacity(65536, test_file);
+    let mut line = Vec::with_capacity(8192);
+    // 0 bytes read is how the OS indicates that we reached end of file
+    while reader.read_until('\n' as u8 ,&mut line)? != 0 {
+        // withstand wacky Windows \r\n line endings
+        if line.last() == Some(&('\r' as u8)) {
+            line.pop();
+        }
+        let _validated_utf8 = simdutf8::basic::from_utf8(&line);
+        let sum: u64 = line.iter().map(|i| *i as u64).sum();
+        writeln!(writer, "{sum}")?;
+        line.clear(); // clear the buffer after each line, or we'll end up with the whole file in memory!
+    }
+
+    Ok(())
+}
